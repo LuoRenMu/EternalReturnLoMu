84132b8f85d7976a2e2aefdeca2b45599b33f3d7
LoMu <2842775752@qq.com>
2026-08-16 18:59:42 +0800

    Fix(test): 修复资源路径测试的跨平台兼容性

--- common/src/test/kotlin/cn/luorenmu/common/util/ResourceCheckUtilTest.kt
+++ common/src/test/kotlin/cn/luorenmu/common/util/ResourceCheckUtilTest.kt
@@ -18,8 +18,8 @@ class ResourceCheckUtilTest {
     @Test
     fun normalizesPathsAndOnlyCachesNonEmptyFiles() {
         val directory = Files.createTempDirectory("resource-check-test")
-        val file = directory.resolve("nested").resolve("..").resolve("image.png")
-        Files.createDirectories(file.parent)
+        val nestedDirectory = Files.createDirectories(directory.resolve("nested"))
+        val file = nestedDirectory.resolve("..").resolve("image.png")
         Files.createFile(file)
         assertFalse(ResourceCheckUtil.checkResource(file))
 
